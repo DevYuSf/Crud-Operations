a0546d352a9d2183e69bc86843f37f7517567d0d
DevYuSf <yusufisxq@gmail.com>
2026-01-07 21:36:39 +0300

    fix: comment out console log for USER environment variable in db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 console.log(process.env.HOST);
-console.log(process.env.USER);
+// console.log(process.env.USER);
 const db = mysql2.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
