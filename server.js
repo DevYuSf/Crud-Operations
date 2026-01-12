b5b783dd643418154b8076018e4c83217d8b403e
DevYuSf <yusufisxq@gmail.com>
2026-01-12 20:46:05 +0300

    adding mongo user version

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import db from './config/db.js';
 import router from './router/course.route.js';
-
+//mongo import routes
+import { createUser,getAllUsers,getUserById,login } from './controller/mongo/user.controller.js';
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ db;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+//mongo routes
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
